dc4fcc02354d19b356b063380361f6b29b3bf91f
Sarah Loui <ssarahloui@gmail.com>
2022-10-06 20:33:25 +0000

    Update database seeds file to contain one initial product

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Product.create(name:        "Reformation Cassidy Denim Jumpsuit",
+                description:"Reformation Cassidy Denim Jumpsuit in Indio. Size 8. New with tag.",
+                price:      19800).image.attach(
+                                    io: File.open('app/assets/images/cassidydenimjumpsuit1.png'),
+                                    filename: 'cassidydenimjumpsuit1.png',
+                                    content_type: 'image/png')
