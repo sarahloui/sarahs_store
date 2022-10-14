d025e4a14b21cddc337b646218409ad8fc72a94f
Sarah Loui <ssarahloui@gmail.com>
2022-10-14 22:00:50 +0000

    Add expiring cart functionality and product hold status

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -8,16 +8,35 @@ class OrdersController < ApplicationController
     # Find Product to Buy
     product = Product.find(params[:product_id])
 
-    #Check that product is not already associated with an order
+     #Check that product is not already associated with an order
     if product.order!=nil
-      flash[:alert] = "Sorry, your item is not available for purchase"
-      redirect_to root_url and return
+      #Check that product hasn't been purchased already
+      if product.status == "sold"
+        flash[:alert] = "Sorry, the item that you selected is sold and is no longer available for purchase"
+        redirect_to root_url and return
+      elsif product.status =="on hold"
+        #Product has been in other users cart for less than 5 minutes
+        if product.order.created_at.since(300)>DateTime.now
+          flash[:alert] = "The item you selected is currently on hold, please try again in a few minutes"
+          redirect_to root_url and return
+        else
+          #expire checkout session and delete order if 5 minutes has elasped
+          #Find matching checkout session and expire it if it's still open
+          checkout_session_to_expire =Stripe::Checkout::Session.retrieve(product.order.checkout_session)
+          if checkout_session_to_expire.status == "open"
+            Stripe::Checkout::Session.expire(product.order.checkout_session)
+          end
+          product.order.delete
+          product.update(status: "available")
+        end
+      end
     end
 
     # New order with matching product
     order = product.build_order
 
     if order.save
+      product.update(status: "on hold")
       checkout_session = Stripe::Checkout::Session.create({
         client_reference_id: order.id,
         phone_number_collection: {
@@ -68,6 +87,7 @@ class OrdersController < ApplicationController
     @item_sold = Stripe::Checkout::Session.list_line_items(params[:session_id]).data[0]
     order = Order.find_by(id: @completed_checkout_session.client_reference_id)
     if (order.checkout_session == @completed_checkout_session.id)
+      product = order.product
       if !order.update(name: @completed_checkout_session.shipping_details.name,
                       address_line1: @completed_checkout_session.shipping_details.address.line1,
                       address_line2: @completed_checkout_session.shipping_details.address.line2,
@@ -76,7 +96,7 @@ class OrdersController < ApplicationController
                       address_zip: @completed_checkout_session.shipping_details.address.postal_code,
                       email: @completed_checkout_session.customer_details.email,
                       phone_number: @completed_checkout_session.customer_details.phone,
-                      status: @completed_checkout_session.payment_status)
+                      status: @completed_checkout_session.payment_status) || !product.update(status: "sold")
         flash[:alert]="Order could not be saved"
         redirect_to root_url
       end
