fc8d5eb0a2268db3074ada5ff249247fd2074412
Sarah Loui <ssarahloui@gmail.com>
2022-11-16 00:51:36 -1000

    Add formatted_price helper for products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,5 +6,4 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
   end
-
 end
--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,5 @@
 module ProductsHelper
+  def formatted_price(price)
+    number_to_currency(price/100.00)
+  end
 end
