2732a6767c7dbb06ec7575901e43e8277b774b63
Julian Fahrer <julian@fahrer.net>
2023-01-12 15:43:49 -0800

    Add a really cool test

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe Product, :type => :model do
   it "is valid with valid attributes" do
     expect(Product.new).to be_valid
   end
+
+  it "is valid with valid attributes" do
+    true
+  end
 end
