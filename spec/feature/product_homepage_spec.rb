aab8ccb1ae988f5d49a6cbf6178b7e12d52a5c78
Sarah Loui <ssarahloui@gmail.com>
2023-01-30 14:33:09 -1000

    add homepage tests

--- spec/feature/product_homepage_spec.rb
+++ spec/feature/product_homepage_spec.rb
@@ -2,10 +2,32 @@ require 'rails_helper'
 
 describe "the store homepage", type: :feature do
   before :each do
-    @product1 = Product.create!(name: "Apron", description: "Red apron", price: 1234, status: "available")
-    @product2 = Product.create!(name: "Blouse", description: "Silk blouse", price: 5678, status: "available")
-    @product3 = Product.create!(name: "Coat", description: "Winter coat", price: 9900, status: "available" )
+    @product1 = Product.create!(name: "Apron", description: "Red apron", price: 1234, number_sold:0)
+    @product2 = Product.create!(name: "Blouse", description: "Silk blouse", price: 5678, number_sold:0)
+    @product3 = Product.create!(name: "Coat", description: "Winter coat", price: 9900, number_sold:0)
+    @product4 = Product.create!(name: "Dress", description: "Denim dress", price: 10000, number_sold: 0)
   end
+
+  context "when there are no current offers" do
+    it "doesn't display an offer" do
+      visit root_url
+      expect(page).to have_text("No offer is currently available")
+    end
+  end
+
+  context "when a current offer exists" do
+    it "displays the current offer with correct product details" do
+      @product1.offers.create!(status: "available", current: "true")
+      visit root_url
+      expect(page).to have_text("Apron")
+      expect(page).to have_text("Description: Red apron")
+      expect(page).to have_text("Price: $12.34")
+      expect(page).to have_button("Buy Now!")
+    end
+  end
+end
+
+=begin
 
   context "when all products are available" do
     it "displays the first unsold product" do
@@ -42,4 +64,4 @@ describe "the store homepage", type: :feature do
       expect(page).to have_text("Apron")
     end
   end
-end
+=end
